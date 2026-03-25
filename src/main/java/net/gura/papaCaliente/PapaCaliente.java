6973ee462a89d172ca42ee11056287c88e43ae51
Gura <contactogura1@gmail.com>
2026-03-25 09:33:51 -0500

    feat: add tab completion for papacaliente command
    - Implement TabCompleter interface with subcommand suggestions
    - Add player name completion for add/remove commands
    - Add GUI type completion for gui subcommand
    - Register tab completer in main plugin class

--- src/main/java/net/gura/papaCaliente/PapaCaliente.java
+++ src/main/java/net/gura/papaCaliente/PapaCaliente.java
@@ -1,15 +1,19 @@
 package net.gura.papaCaliente;
 
+import lombok.Getter;
 import net.gura.papaCaliente.commands.PapaCalienteCommand;
 import net.gura.papaCaliente.game.GameManager;
 import net.gura.papaCaliente.game.GamePlayer;
 import net.gura.papaCaliente.listeners.*;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PapaCaliente extends JavaPlugin {
 
+    @Getter
     public static PapaCaliente plugin;
+    @Getter
     private GameManager gameManager;
 
     String version = Bukkit.getVersion();
@@ -32,23 +36,24 @@ public final class PapaCaliente extends JavaPlugin {
         getServer().getConsoleSender().sendMessage("Made by Gura1");
     }
 
-    public static PapaCaliente getPlugin() {
-        return plugin;
-    }
-
-    public GameManager getGameManager() {
-        return gameManager;
-    }
-
     @Override
     public void onDisable() {
 
+        if (gameManager != null && gameManager.isRunning()) {
+            gameManager.stopGame();
+        }
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.setGlowing(false);
+        }
         getServer().getConsoleSender().sendMessage("Evento Papa Caliente Deshabilitado " + "Versión " + version);
         getServer().getConsoleSender().sendMessage("Made by Gura1");
     }
 
     public void registerCommands() {
         //Crear un handler para que maneje el comando /papacaliente
-        getCommand("papacaliente").setExecutor(new PapaCalienteCommand());
+        PapaCalienteCommand cmdHandler = new PapaCalienteCommand();
+        getCommand("papacaliente").setExecutor(cmdHandler);
+        getCommand("papacaliente").setTabCompleter(cmdHandler);
     }
 }
